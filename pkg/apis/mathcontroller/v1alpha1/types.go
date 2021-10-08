c036179c1f8df8c2ef20497492e0a79bb472d615
PRIYA KUMARI <PRIYA.KUMARI1@tatacommunications.com>
2021-10-08 15:09:14 +0530

    commit2

---
+++ pkg/apis/mathcontroller/v1alpha1/types.go
@@ -0,0 +1,30 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type Math struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec MathSpec `json:"spec"`
+}
+
+type MathSpec struct {
+	Number1 int32 `json:"number1"`
+	Number2 int32 `json:"number2"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// FooList is a list of Foo resources
+type MathList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Math `json:"items"`
+}
