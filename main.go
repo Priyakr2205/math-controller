87883d7a24e352151bfe11f770198f15fc573e9e
PRIYA KUMARI <PRIYA.KUMARI1@tatacommunications.com>
2021-10-12 13:42:01 +0530

    modifying main.go

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	//      "flag"
+	"flag"
 	"fmt"
-	"os"
 
-	//      "time"
+	//"os"
 
-	//      kubeinformers "k8s.io/client-go/informers"
+	"time"
+
+	//kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
@@ -17,26 +19,21 @@ import (
 
 	//      "k8s.io/sample-controller/pkg/signals"
 	clientset "math-controller/pkg/client/clientset/versioned"
-	//informers "math-controller/pkg/generated/informers/externalversions"
-	"context"
+	informers "math-controller/pkg/client/informers/externalversions"
+	"math-controller/pkg/signals"
+)
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+var (
+	masterURL  string
+	kubeconfig string
 )
 
 func main() {
 
-	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
 	if err != nil {
 		klog.Fatalf("getClusterConfig: %v", err)
 	}
-
-	// generate the client based off of the config
-	//  client, err := kubernetes.NewForConfig(config)
-	// if err != nil {
-	//   klog.Fatalf("getClusterConfig: %v", err)
-	// }
 
 	kubeClient, err := kubernetes.NewForConfig(config)
 	if err != nil {
@@ -54,7 +51,28 @@ func main() {
 	//creating sharedinformers for custom resource
 	//exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
 
-	pods, err := kubeClient.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+	/*pods, err := kubeClient.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
 	fmt.Println(pods)
+	*/
 
+	exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+	//kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+
+	controller := NewController(kubeClient, exampleClient,
+		exampleInformerFactory.Mathematics().V1alpha1().Maths())
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	exampleInformerFactory.Start(stopCh)
+	err = controller.Run(3, stopCh)
+	if err != nil {
+		fmt.Println("Controller exited")
+	}
+
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 }
